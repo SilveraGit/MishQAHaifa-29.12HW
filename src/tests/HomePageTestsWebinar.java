1b27728b4b850339ca77c4ca67978bf51a74c828
MarinaA2015 <m.abrashkina2015@gmail.com>
2018-12-24 21:13:58 +0200

    class HomePageTestsWebinar was refactored and were created new classes:  HomePageHelperWebinar, EventsUnAuthPageHelperWebinar

--- src/tests/HomePageTestsWebinar.java
+++ src/tests/HomePageTestsWebinar.java
@@ -4,10 +4,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pages.EventUnAuthPageHelperWebinar;
+import pages.EventsUnAuthPageHelper;
+import pages.HomePageHelper;
+import pages.HomePageHelperWebinar;
 
 import java.util.List;
 
@@ -15,32 +21,36 @@ import java.util.List;
  * Created by Inka on 17-Dec-18.
  */
 public class HomePageTestsWebinar extends TestBase {
+    HomePageHelperWebinar homePageWebinar;
+    EventUnAuthPageHelperWebinar eventUnAuthPageHelperWebinar;
+
+    @BeforeMethod
+    public void initPage(){
+        homePageWebinar = PageFactory
+                .initElements(driver, HomePageHelperWebinar.class);
+        eventUnAuthPageHelperWebinar = PageFactory
+                .initElements(driver, EventUnAuthPageHelperWebinar.class);
+
+    }
+
     @Test
     public void openHomePageTest()  {
-
-        waitUntilElementIsLoaded(driver,By.className("mat-stroked-button"),20);
-        waitUntilElementIsLoaded(driver,By.xpath("//span[contains(text(),'Create Account')]"),20);
-        List<WebElement> topLinksList = driver.findElements(By.cssSelector(".mat-button-wrapper .mat-button-wrapper"));
-        WebElement createAccountButton = topLinksList.get(1);
-        WebElement goToEventsButton = driver.findElement(
-            By.className("mat-stroked-button"));
-        System.out.println("Text of button: " + goToEventsButton.getText());
-        Assert.assertTrue(createAccountButton.getText().equals("Create Account"));
+        homePageWebinar.waitUntilPageIsLoaded();
+        String createAccountButtonName = homePageWebinar.getCreateAccountButtonName();
+        Assert.assertEquals("Create Account",createAccountButtonName);
     }
 
     @Test
     public void goToEventButtonTest()  {
+        homePageWebinar.waitUntilPageIsLoaded();
+        homePageWebinar.pressGoToEventButton();
+        eventUnAuthPageHelperWebinar.waitUntilPageIsLoaded();
+        String loginButtonName = eventUnAuthPageHelperWebinar.getLoginButtonName();
+        String headerFindEvents = eventUnAuthPageHelperWebinar.getHeader();
 
-        waitUntilElementIsLoaded(driver,By.xpath("//span[contains(text(),'Create Account')]"),30);
-        WebElement goToEventsButton = driver.findElement(
-                By.className("mat-stroked-button"));
-        goToEventsButton.click();
-        waitUntilElementIsLoaded(driver,By.xpath("//button[@class='mat-raised-button']"),20);
-        List<WebElement> topLinksList = driver.findElements(By.cssSelector(".mat-button-wrapper .mat-button-wrapper"));
-        WebElement loginButton = topLinksList.get(0);
-        WebElement titleEventsPage = driver.findElement(By.xpath("//h1[@class='gorisontal-center']"));
-        Assert.assertTrue(titleEventsPage.getText().equals("Find event"));
-        Assert.assertTrue(loginButton.getText().equals("Login"));
+
+        Assert.assertEquals("Find event",headerFindEvents);
+        Assert.assertEquals("Login", loginButtonName);
     }
 
    }
