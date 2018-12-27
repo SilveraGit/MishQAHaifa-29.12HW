63d1dc1aa78933623dcad8cea0756efea36ad298
MarinaA2015 <m.abrashkina2015@gmail.com>
2018-12-27 21:52:08 +0200

    Webinar - refactoring (was added @FindBy for HomePageHelper, created the others "Helper" classes )

---
+++ src/pages/LoginPageHelperWebinar.java
@@ -0,0 +1,48 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+/**
+ * Created by Inka on 27-Dec-18.
+ */
+public class LoginPageHelperWebinar extends PageBase{
+
+    public LoginPageHelperWebinar(WebDriver driver) {
+        super(driver);
+    }
+
+    public void waitUntilPageIsLoaded() {
+        waitUntilElementIsLoaded(driver, By.xpath("//button[@aria-label='Close dialog']"),20);
+    }
+
+
+    public void enterValueToFieldEmail(String value) {
+        WebElement email_field = driver.findElement(By
+                .xpath("//input[@formcontrolname='email']"));
+        email_field.click();
+        email_field.sendKeys(value);
+
+    }
+
+    public void enterValueToFieldPassword(String value) {
+        WebElement password_field = driver.findElement(By
+                .xpath("//input[@formcontrolname='password']"));
+        password_field.click();
+        password_field.sendKeys(value);
+    }
+
+    public void pressLogInButton() {
+        waitUntilElementIsLoaded(driver, By.xpath("//span[contains(text(),'Log in')]"),30);
+        WebElement login_button=driver.findElement(By
+                .xpath("//span[contains(text(),'Log in')]"));
+        login_button.click();
+    }
+
+    public boolean isAlertTextCorrect(String value) {
+        waitUntilElementIsLoaded(driver, By.xpath("//div[@class='alert alert-danger ng-star-inserted']"),20);
+        WebElement alertText = driver.findElement(By.xpath(("//div[@class='alert alert-danger ng-star-inserted']")));
+        return alertText.getText().equals(value);
+    }
+}
--- src/tests/LoginPageTestsWebinar.java
+++ src/tests/LoginPageTestsWebinar.java
@@ -4,67 +4,49 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pages.*;
 
 /**
  * Created by Inka on 20-Dec-18.
  */
 public class LoginPageTestsWebinar extends TestBase{
+    HomePageHelperWebinar homePageWebinar;
+    LoginPageHelperWebinar loginPageWebinar;
+    EventAuthPageHelperWebinar eventAuthPagerWebinar;
+
+    @BeforeMethod
+    public void initPage(){
+        homePageWebinar = PageFactory
+                .initElements(driver, HomePageHelperWebinar.class);
+        loginPageWebinar = PageFactory.initElements(driver, LoginPageHelperWebinar.class);
+        eventAuthPagerWebinar = PageFactory.initElements(driver, EventAuthPageHelperWebinar.class);
+    }
+
     @Test
     public void loginPositive() {
-        waitUntilElementIsLoaded(driver,By.className("mat-stroked-button"),20);
-        waitUntilElementIsLoaded(driver,By.xpath("//span[contains(text(),'Create Account')]"),20);
-        waitUntilElementIsLoaded(driver, By.xpath("//span[contains(text(),'Login')]"),30);
-        WebElement login = driver.findElement(By
-                .xpath("//span[contains(text(),'Login')]"));
-        login.click();
-        waitUntilElementIsLoaded(driver,By.xpath("//button[@aria-label='Close dialog']"),20);
-        WebElement email_field = driver.findElement(By
-                .xpath("//input[@formcontrolname='email']"));
-        WebElement password_field = driver.findElement(By
-                .xpath("//input[@formcontrolname='password']"));
-        email_field.click();
-        email_field.sendKeys("mishUser1@gmail.com");
-        password_field.click();
-        password_field.sendKeys("example");
-        waitUntilElementIsLoaded(driver, By.xpath("//span[contains(text(),'Log in')]"),30);
-        WebElement login_button=driver.findElement(By
-                .xpath("//span[contains(text(),'Log in')]"));
-        login_button.click();
-        waitUntilElementIsLoaded(driver, By.xpath("//mat-icon[@class='but mat-icon material-icons']"),20);
-        WebElement iconMenu = driver.findElement(By.xpath("//mat-icon[@class='but mat-icon material-icons']"));
-        Assert.assertTrue(iconMenu.getAttribute("mattooltip").equals("Menu"));
+        homePageWebinar.waitUntilPageIsLoaded();
+        homePageWebinar.pressLoginButton();
+        loginPageWebinar.waitUntilPageIsLoaded();
+        loginPageWebinar.enterValueToFieldEmail("mishUser1@gmail.com");
+        loginPageWebinar.enterValueToFieldPassword("example");
+        loginPageWebinar.pressLogInButton();
+        eventAuthPagerWebinar.waitUntilPageIsLoaded();
+        Assert.assertEquals("Menu",eventAuthPagerWebinar.getToolTipIconMenu());
+        Assert.assertTrue(eventAuthPagerWebinar.isHeaderPageCorrect("Find event"));
     }
 
     @Test
     public void negativeLogin(){
-        //driver.get("https://mish.sheygam.com/#/wellcome");
-        waitUntilElementIsLoaded(driver,
-                By.xpath("//span[contains(text(),'Login')]"),
-                //By.xpath("//span[contains(text(),'Login')]"),
-                45);
-        //waitUntilElementIsLoaded(driver,By.className("mat-stroked-button"),20);
-        //waitUntilElementIsLoaded(driver,By.xpath("//span[contains(text(),'Create Account')]"),30);
-        //waitUntilElementIsLoaded(driver, By.xpath("//span[contains(text(),'Login')]"),50);
-        WebElement login = driver.findElement(By
-                .xpath("//span[contains(text(),'Login')]"));
-        login.click();
-        waitUntilElementIsLoaded(driver,By.xpath("//button[@aria-label='Close dialog']"),20);
-        WebElement email_field = driver.findElement(By
-                .xpath("//input[@formcontrolname='email']"));
-        WebElement password_field = driver.findElement(By
-                .xpath("//input[@formcontrolname='password']"));
-        email_field.click();
-        email_field.sendKeys("mrn@123.com");
-        password_field.click();
-        password_field.sendKeys("marina");
-        waitUntilElementIsLoaded(driver, By.xpath("//span[contains(text(),'Log in')]"),20);
-        WebElement login_button=driver.findElement(By
-                .xpath("//span[contains(text(),'Log in')]"));
-        login_button.click();
-        waitUntilElementIsLoaded(driver, By.xpath("//div[@class='alert alert-danger ng-star-inserted']"),20);
-        WebElement alertText = driver.findElement(By.xpath(("//div[@class='alert alert-danger ng-star-inserted']")));
-        Assert.assertTrue(alertText.getText().equals("Wrong authorization, login or password"));
+        homePageWebinar.waitUntilPageIsLoaded();
+        homePageWebinar.pressLoginButton();
+        loginPageWebinar.waitUntilPageIsLoaded();
+        loginPageWebinar.enterValueToFieldEmail("misshUser1@gmail.com");
+        loginPageWebinar.enterValueToFieldPassword("example");
+        loginPageWebinar.pressLogInButton();
+        Assert.assertTrue(loginPageWebinar.isAlertTextCorrect("Wrong authorization, login or password"));
     }
 }
